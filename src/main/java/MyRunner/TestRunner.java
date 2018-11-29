0ac4279015bd7cf3f6ad53bed2b6f1e9a3e727e2
yugchary <42136297+yugchary@users.noreply.github.com>
2018-11-29 15:38:03 +0530

    first

---
+++ src/main/java/MyRunner/TestRunner.java
@@ -0,0 +1,23 @@
+package MyRunner;
+
+import org.junit.runner.RunWith;
+
+
+/*
+@CucumberOptions(
+		features = "C:\\Users\\akkyu01\\eclipse-workspace\\JavaTraining\\FreeCRMBDD\\src\\main\\java\\Features\\login.feature", //the path of the feature files
+		glue={"stepDefinitions"} //the path of the step definition files
+		
+				
+		)
+*/
+
+
+
+public class TestRunner {
+
+	
+	
+	 
+	
+}
---
+++ target/classes/Features/login.feature
@@ -0,0 +1,8 @@
+Feature: Free CRM Login Feature
+
+Scenario Outline: Free CRM Login Test Scenario
+
+Given user is already on Login Page
+When title of login page is Free CRM
+Then user clicks on login button
+Then user is on home page
